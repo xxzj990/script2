2aeef2755ddad73195f994f19ec9d072a483383d
dzhl <47837136+dzhl@users.noreply.github.com>
2020-05-28 08:48:47 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O gc.sh https://raw.githubusercontent.com/dzhl/script/master/gc.sh && chmod +x gc.sh && ./gc.sh
+wget -O ddnspod.sh https://raw.githubusercontent.com/dzhl/script/master/ddnspod.sh && chmod +x ddnspod.sh && ./ddnspod.sh
