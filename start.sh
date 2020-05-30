f59ab6d3b8f4d6c61081856bbc43be663a779543
dzhl <47837136+dzhl@users.noreply.github.com>
2020-05-30 11:10:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-wget -O ddnspod.sh https://raw.githubusercontent.com/dzhl/script/master/ardnspod && chmod +x ardnspod && ./ardnspod
+apt-get install screen
+ln -s /home/rclone/rclone /usr/bin/rclone
+chmod +x /home/rclone/rclone
+chmod +x /home/rclone/rclone.sh
+rclone config --config /home/rclone/rclone.conf
+/home/rclone/rclone.sh start
+wget -O ardnspod https://raw.githubusercontent.com/dzhl/script/master/ardnspod && chmod +x ardnspod && ./ardnspod
 wget -O gc.sh https://raw.githubusercontent.com/dzhl/script/master/gc.sh && chmod +x gc.sh && ./gc.sh
