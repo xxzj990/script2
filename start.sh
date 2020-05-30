ff697a06169ee0b72e3043eea97de3a862db5a0e
dzhl <47837136+dzhl@users.noreply.github.com>
2020-05-30 10:23:31 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -O ddnspod.sh https://raw.githubusercontent.com/dzhl/script/master/ddnspod.sh && chmod +x ddnspod.sh && ./ddnspod.sh
-wget -O dns.conf https://raw.githubusercontent.com/dzhl/script/master/dns.conf
-wget -O gc.sh https://raw.githubusercontent.com/dzhl/script/master/gc.sh && chmod +x gc.sh && ./gc.sh
