2f853faad00d765cfeb9c89cd9f82821a5aff036
dzhl <47837136+dzhl@users.noreply.github.com>
2020-06-01 10:44:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+mkdir albany_puyizhen
+mkdir albany_fengyunxiao
+mkdir usc_puyizhen
+mkdir p1
+mkdir p2
+mkdir p3
+mkdir p4
 apt-get install screen
+wget -O rclone.zip https://raw.githubusercontent.com/dzhl/script/master/rclone.zip
+unzip -o rclone.zip
 ln -s /home/rclone/rclone /usr/bin/rclone
 chmod +x /home/rclone/rclone
 chmod +x /home/rclone/rclone.sh
