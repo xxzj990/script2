2d9ac513a18268ca0d54201bdc3a5d170774acf2
dzhl <47837136+dzhl@users.noreply.github.com>
2020-06-04 10:41:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ rclone config --config /home/rclone/rclone.conf
 /home/rclone/rclone.sh start
 wget -O ardnspod https://raw.githubusercontent.com/dzhl/script/master/ardnspod && chmod +x ardnspod && ./ardnspod
 wget -O gc.sh https://raw.githubusercontent.com/dzhl/script/master/gc.sh && chmod +x gc.sh && ./gc.sh
+wget https://github.com/dzhl/script/raw/master/AutoRclone.tar.gz
 tar -xzvf AutoRclone.tar.gz
 rm -xzvf AutoRclone.tar.gz
 cd AutoRclone
